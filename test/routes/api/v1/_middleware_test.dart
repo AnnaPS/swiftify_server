a2af3142f6a491534c9393ea3165611fefa4255c
Ana Polo <anna-91_@hotmail.com>
2025-01-14 15:12:42 +0100

    test: middleware tests

---
+++ test/routes/api/v1/_middleware_test.dart
@@ -0,0 +1,43 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:swiftify_data_repository/swiftify_data_repository.dart';
+import 'package:swiftify_data_source/swiftify_data_source.dart';
+import 'package:test/test.dart';
+
+import '../../../../routes/api/v1/_middleware.dart';
+
+class _MockRequestContext extends Mock implements RequestContext {}
+
+class _MockSwiftifyDataRepository extends Mock
+    implements SwiftifyDataRepository {}
+
+void main() {
+  group('middleware', () {
+    late SwiftifyDataRepository swiftifyDataRepository;
+    setUp(() {
+      swiftifyDataRepository = _MockSwiftifyDataRepository();
+    });
+
+    test('provides SwiftifyDataSource', () async {
+      final handler = middleware((_) => Response());
+      final request = Request.get(Uri.parse('http://localhost/'));
+      final context = _MockRequestContext();
+
+      when(() => context.request).thenReturn(request);
+      when(() => context.provide<SwiftifyDataSource>(any()))
+          .thenReturn(context);
+
+      when(() => context.read<SwiftifyDataRepository>())
+          .thenReturn(swiftifyDataRepository);
+
+      await handler(context);
+
+      final create =
+          verify(() => context.provide<SwiftifyDataSource>(captureAny()))
+              .captured
+              .single as SwiftifyDataSource Function();
+
+      expect(create(), isA<SwiftifyDataSource>());
+    });
+  });
+}
